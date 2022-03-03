a437cc96442b9740db69364fe55dfa5588014eae
Matis Rammi <matis.rammi@khk.ee>
2022-03-03 12:52:45 +0200

    Connected to MongoDB

--- index.js
+++ index.js
@@ -2,11 +2,36 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 
+const mongoose = require("mongoose");
+
+// MongoDB account data
+require("dotenv").config();
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const cluster = process.env.CLUSTER;
+const dbname = process.env.DBNAME;
+
+// Connect to MongoDB
+mongoose.connect(`mongodb+srv://${username}:${password}@${cluster}.tpfx3.mongodb.net/${dbname}?retryWrites=true&w=majority`,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+
+// Check MongoDB connection
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+    console.log("Connected successfully");
+});
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(PORT, function() {
+    console.log(`Server started on port ${PORT}`);
+});
 
